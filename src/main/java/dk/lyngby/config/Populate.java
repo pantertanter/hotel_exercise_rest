49d3b398148ac1c2f64fb4100c7085a3b155c2f0
Alex <Alexanderstubmichelsen@gmail.com>
2024-06-15 11:11:44 +0200

    admin added to populate

--- src/main/java/dk/lyngby/config/Populate.java
+++ src/main/java/dk/lyngby/config/Populate.java
@@ -1,12 +1,8 @@
 package dk.lyngby.config;
 
-
 import dk.lyngby.model.*;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import org.jetbrains.annotations.NotNull;
-
-import java.math.BigDecimal;
-import java.util.Set;
 
 public class Populate {
     public static void main(String[] args) {
@@ -15,17 +11,33 @@ public class Populate {
 
         try (var em = emf.createEntityManager()) {
             em.getTransaction().begin();
+
+            // Delete all existing records from tables
+            em.createQuery("DELETE FROM Rating").executeUpdate();
+            em.createQuery("DELETE FROM Picture").executeUpdate();
+            em.createQuery("DELETE FROM User").executeUpdate();
+            em.createQuery("DELETE FROM Role").executeUpdate();
+
+
+            // Persist new data
             Picture picture = new Picture("https://images.unsplash.com/photo-1701906268416-b461ec4caa34?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfGFsbHwyfHx8fHx8Mnx8MTcwMjI5MzE1Mnw&ixlib=rb-4.0.3&q=80&w=400", "the sun is setting over the clouds in the sky");
             Rating rating = new Rating(3);
             picture.addRating(rating);
             User user4 = new User("user4", "user4");
-            Role user = new Role("user");
+            User admin = new User("admin", "admin");
+            Role userRole = new Role("user");
+            Role adminRole = new Role("admin");
             user4.addPicture(picture);
-            user4.addRole(user);
+            user4.addRole(userRole);
+            admin.addRole(adminRole);
             em.persist(rating);
             em.persist(user4);
-            em.persist(user);
+            em.persist(admin);
+            em.persist(userRole);
+            em.persist(adminRole);
             em.persist(picture);
+
+
             em.getTransaction().commit();
         }
     }
