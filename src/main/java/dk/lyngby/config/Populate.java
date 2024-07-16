324c70253d3a43765db15a927f8eb7ad62389d74
Alex <Alexanderstubmichelsen@gmail.com>
2024-07-16 13:04:36 +0200

    Populate

--- src/main/java/dk/lyngby/config/Populate.java
+++ src/main/java/dk/lyngby/config/Populate.java
@@ -18,25 +18,66 @@ public class Populate {
             em.createQuery("DELETE FROM User").executeUpdate();
             em.createQuery("DELETE FROM Role").executeUpdate();
 
-
             // Persist new data
-            Picture picture = new Picture("https://images.unsplash.com/photo-1701906268416-b461ec4caa34?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfGFsbHwyfHx8fHx8Mnx8MTcwMjI5MzE1Mnw&ixlib=rb-4.0.3&q=80&w=400", "the sun is setting over the clouds in the sky", "John Doe", "john_doe", "https://unsplash.com/@johndoe", "https://unsplash.com/@johndoe");
-            User user4 = new User("user4", "user4");
+            Picture picture_1 = new Picture("https://images.unsplash.com/photo-1611288870280-4a322b8ec7ec?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfHNlYXJjaHwxfHx3cmVuY2h8ZW58MHx8fHwxNzIxMTE1NDUyfDA&ixlib=rb-4.0.3&q=80&w=400", "silver steel tool on white surface", "Recha Oktaviani", "rechaoktaviani", "https://unsplash.com/@rechaoktaviani", "https://api.unsplash.com/photos/t__61ap00Mc/download?ixid=M3w1Mzk1MTR8MHwxfHNlYXJjaHwxfHx3cmVuY2h8ZW58MHx8fHwxNzIxMTE1NDUyfDA");
+            Picture picture_2 = new Picture("https://images.unsplash.com/photo-1720719908968-f6bcf72b8ce2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfGFsbHw0fHx8fHx8Mnx8MTcyMDgwNDg5MHw&ixlib=rb-4.0.3&q=80&w=400", "A red building with red balconies and a clock", "Pascal Bullan", "jetztabertempo", "https://unsplash.com/@jetztabertempo", "https://api.unsplash.com/photos/WsdsKOTNEA0/download?ixid=M3w1Mzk1MTR8MHwxfGFsbHw0fHx8fHx8Mnx8MTcyMDgwNDg5MHw");
+            Picture picture_3 = new Picture("https://images.unsplash.com/photo-1720766595377-b413ce3a6478?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfGFsbHw1fHx8fHx8Mnx8MTcyMDgwNDg5MHw&ixlib=rb-4.0.3&q=80&w=400", "A person standing in front of a bush with white flowers", "Max Ovcharenko", "chestmax", "https://unsplash.com/@chestmax", "https://api.unsplash.com/photos/PH-kwn9zkf8/download?ixid=M3w1Mzk1MTR8MHwxfGFsbHw1fHx8fHx8Mnx8MTcyMDgwNDg5MHw");
+            Picture picture_4 = new Picture("https://images.unsplash.com/photo-1720766255393-60ca5199def4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfGFsbHw2fHx8fHx8Mnx8MTcyMDgwNDg5MHw&ixlib=rb-4.0.3&q=80&w=400", "Three pink roses in a vase on a black background", "ISO10", "isoten", "https://unsplash.com/@isoten", "https://api.unsplash.com/photos/MJJ8p9b2AvY/download?ixid=M3w1Mzk1MTR8MHwxfGFsbHw2fHx8fHx8Mnx8MTcyMDgwNDg5MHw");
+            Picture picture_5 = new Picture("https://images.unsplash.com/photo-1721023672487-906b07479bff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfGFsbHwyfHx8fHx8Mnx8MTcyMTExNTQzOXw&ixlib=rb-4.0.3&q=80&w=400", "A wooden table topped with a vase filled with flowers", "Parker Coffman", "lowmurmer", "https://unsplash.com/@lowmurmer", "https://api.unsplash.com/photos/jGDqRxc_xQ8/download?ixid=M3w1Mzk1MTR8MHwxfGFsbHwyfHx8fHx8Mnx8MTcyMTExNTQzOXw");
+            Picture picture_6 = new Picture("https://images.unsplash.com/photo-1720887237185-6b8d5c01005d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mzk1MTR8MHwxfGFsbHw4fHx8fHx8Mnx8MTcyMTExNTQzOXw&ixlib=rb-4.0.3&q=80&w=400", "A couple of people that are looking in a window", "James Chan", "jvmesc_", "https://unsplash.com/@jvmesc_", "https://api.unsplash.com/photos/KWocLB1EHIc/download?ixid=M3w1Mzk1MTR8MHwxfGFsbHw4fHx8fHx8Mnx8MTcyMTExNTQzOXw");
+
+            User user_1 = new User("user_1", "user_1");
+            User user_2 = new User("user_2", "user_2");
+
             User admin = new User("admin", "admin");
             Role userRole = new Role("user");
             Role adminRole = new Role("admin");
-            user4.addPicture(picture);
-            user4.addRole(userRole);
+
+            user_1.addPicture(picture_1);
+            user_1.addPicture(picture_2);
+            user_2.addPicture(picture_3);
+            user_2.addPicture(picture_4);
+            admin.addPicture(picture_5);
+            admin.addPicture(picture_6);
+
+            user_1.addRole(userRole);
+            user_2.addRole(userRole);
             admin.addRole(adminRole);
-            Rating rating = new Rating(3, user4);
-            picture.addRating(rating);
-            em.persist(rating);
-            em.persist(user4);
+
+            Rating rating_1 = new Rating(3, user_1);
+            Rating rating_2 = new Rating(4, user_1);
+            Rating rating_3 = new Rating(5, user_2);
+            Rating rating_4 = new Rating(2, user_2);
+            Rating rating_5 = new Rating(1, admin);
+            Rating rating_6 = new Rating(5, admin);
+
+            picture_1.addRating(rating_1);
+            picture_2.addRating(rating_2);
+            picture_3.addRating(rating_3);
+            picture_4.addRating(rating_4);
+            picture_5.addRating(rating_5);
+            picture_6.addRating(rating_6);
+
+            em.persist(rating_1);
+            em.persist(rating_2);
+            em.persist(rating_3);
+            em.persist(rating_4);
+            em.persist(rating_5);
+            em.persist(rating_6);
+
+            em.persist(user_1);
+            em.persist(user_2);
             em.persist(admin);
+
             em.persist(userRole);
             em.persist(adminRole);
-            em.persist(picture);
 
+            em.persist(picture_1);
+            em.persist(picture_2);
+            em.persist(picture_3);
+            em.persist(picture_4);
+            em.persist(picture_5);
+            em.persist(picture_6);
 
             em.getTransaction().commit();
         }
